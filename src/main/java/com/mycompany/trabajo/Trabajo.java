6eb168ce74169bd0306dbf0349cf768e0857f8b7
lmgom <lmgom@Nina14>
2024-09-05 10:11:17 -0500

    carga 1

---
+++ src/main/java/com/mycompany/trabajo/Trabajo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajo;
+
+/**
+ *
+ * @author lmgom
+ */
+public class Trabajo {
+
+    public static void main(String[] args) {
+        System.out.println("¡Hola Mundo!");
+    }
+}
